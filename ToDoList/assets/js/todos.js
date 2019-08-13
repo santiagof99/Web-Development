33d8defb7167edfdef79889059103c78122898c6
santiagof99 <santiagofrancogarzon@gmail.com>
2019-08-12 21:42:01 -0400

    Add files via upload

---
+++ ToDoList/assets/js/todos.js
@@ -0,0 +1,26 @@
+//check off specific todos by clicking
+$("ul").on("click","li",function(){
+	$(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("ul").on("click","span", function(event){
+	event.stopPropagation();
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+});
+
+$("input[type = 'text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new list and add to ul
+		$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span>"+" "+todoText+"</li>");
+	}
+})
+
+$(".fa-plus").click(function(){
+	$("input[type = 'text']").fadeToggle();
+});
